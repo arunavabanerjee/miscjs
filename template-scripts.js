e07f27c6e928af64d4b129d887a2421100756d47
arunavabanerjee <mrarunbanerjee09@gmail.com>
2017-09-24 14:25:23 +0000

    Create template-scripts.js

---
+++ template-scripts.js
@@ -0,0 +1,16 @@
+
+//window location object
+// If URL is http://www.somedomain.com/account/search?filter=a#top
+
+window.location.pathname // /account/search
+
+// For reference:
+
+window.location.host     // www.somedomain.com (includes port if there is one)
+window.location.hostname // www.somedomain.com
+window.location.hash     // #top
+window.location.href     // http://www.somedomain.com/account/search?filter=a#top
+window.location.port     // (empty string)
+window.location.protocol // http:
+window.location.search   // ?filter=a  
+
